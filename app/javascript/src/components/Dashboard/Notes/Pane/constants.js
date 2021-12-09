322c771d2ad5cb08a43a7a3e6031bb60f2902d00
Thejus Paul <thejuspaul@pm.me>
2021-12-09 07:21:16 +0530

    Added initial values as constants

---
+++ app/javascript/src/components/Dashboard/Notes/Pane/constants.js
@@ -0,0 +1,12 @@
+const INITIAL_CONTACT_VALUES = ["Oliver Smith", "Sam Smith"];
+
+const INITIAL_TAG_VALUES = [
+  "Getting Started",
+  "Onboarding",
+  "User Flow",
+  "UX",
+  "Bugs",
+  "V2",
+];
+
+export { INITIAL_CONTACT_VALUES, INITIAL_TAG_VALUES };
