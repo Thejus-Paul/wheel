32f54df27a8866da5dcb114eb513c556375429ad
Thejus Paul <thejuspaul@pm.me>
2021-12-09 07:24:54 +0530

    Added tags and contact selection to form

--- app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
+++ app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 
+import { Check } from "@bigbinary/neeto-icons";
 import { Formik, Form } from "formik";
 import { Button, Pane } from "neetoui/v2";
-import { Input, Textarea } from "neetoui/v2/formik";
+import { Input, Textarea, Select } from "neetoui/v2/formik";
 
 import notesApi from "apis/notes";
 import formValidationSchemas from "constants/formValidationSchemas";
 
+import { INITIAL_CONTACT_VALUES, INITIAL_TAG_VALUES } from "./constants";
+
 export default function NoteForm({ onClose, refetch, note, isEdit }) {
-  const [submitted, setSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const handleSubmit = async values => {
     try {
-      setSubmitted(true);
+      setIsSubmitted(true);
       if (isEdit) {
         await notesApi.update(note.id, values);
       } else {
@@ -24,12 +27,18 @@ export default function NoteForm({ onClose, refetch, note, isEdit }) {
     }
   };
 
+  const populateSelectOptions = options =>
+    options.map(tag => ({
+      label: tag,
+      value: tag,
+    }));
+
   return (
     <Formik
       initialValues={note}
       onSubmit={handleSubmit}
-      validateOnBlur={submitted}
-      validateOnChange={submitted}
+      validateOnBlur={isSubmitted}
+      validateOnChange={isSubmitted}
       validationSchema={formValidationSchemas.notesForm}
     >
       {({ isSubmitting, handleSubmit }) => (
@@ -39,13 +48,35 @@ export default function NoteForm({ onClose, refetch, note, isEdit }) {
               label="Title"
               name="title"
               className="flex-grow-0 w-full"
+              placeholder="Enter the note title"
+              size="large"
               required
             />
             <Textarea
               label="Description"
               name="description"
               className="flex-grow-0 w-full"
-              rows={8}
+              placeholder="Enter the note description"
+              rows={2}
+              required
+            />
+            <Select
+              label="Assigned Contact"
+              name="contact"
+              className="flex-grow-0 w-full"
+              placeholder="Select contact"
+              isSearchable
+              options={populateSelectOptions(INITIAL_CONTACT_VALUES)}
+              required
+            />
+            <Select
+              label="Tags"
+              name="tags"
+              className="flex-grow-0 w-full"
+              placeholder="Select tags"
+              isMulti
+              isSearchable
+              options={populateSelectOptions(INITIAL_TAG_VALUES)}
               required
             />
           </Pane.Body>
@@ -54,13 +85,14 @@ export default function NoteForm({ onClose, refetch, note, isEdit }) {
               type="submit"
               label={isEdit ? "Update" : "Save Changes"}
               size="large"
+              icon={Check}
               style="primary"
               className="mr-3"
               disabled={isSubmitting}
               loading={isSubmitting}
               onClick={e => {
                 e.preventDefault();
-                setSubmitted(true);
+                setIsSubmitted(true);
                 handleSubmit();
               }}
             />
